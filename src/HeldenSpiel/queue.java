2e243d94b62f277b901ee72c84b0f97658bf76a7
Maximilian Inckmann <43142805+maximiliani@users.noreply.github.com>
2019-10-28 17:28:25 +0100

    Created queue.java
    Allows to add Held to an queue.
    Allows to remove a Held from the queue...
    Allows to get the first Held of the queue...
    Allows to asks if th queue is empty...

---
+++ src/HeldenSpiel/queue.java
@@ -0,0 +1,17 @@
+package HeldenSpiel;
+
+public class queue {
+    private Held[] bank;
+    public queue(){bank = new Held[30];}
+
+    public void addHeld(Held h){for(int i = 0; i<bank.length; i++)if(bank[i]==null) bank[i] = h;}
+
+    public void removeHeld(){
+        for(int i = 0; i<bank.length; i++) bank[i] = bank[i+1];
+        bank[bank.length -1] = null;
+    }
+
+    public Held firstHeld(){return bank[0]; }
+
+    public boolean bankEmpty(){return bank[0]==null;}
+}
