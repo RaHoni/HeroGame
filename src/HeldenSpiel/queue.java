f23211fc15cf067995f06d374b69f7ac627f8088
maximiliani <maincky@gmail.com>
2019-10-30 08:51:12 +0100

    added interruption in addHeld()

--- src/HeldenSpiel/queue.java
+++ src/HeldenSpiel/queue.java
@@ -4,7 +4,12 @@ public class queue {
     private Held[] bank;
     public queue(){bank = new Held[30];}
 
-    public void addHeld(Held h){for(int i = 0; i<bank.length; i++)if(bank[i]==null) bank[i] = h;}
+    public void addHeld(Held h){
+        for(int i = 0; i<bank.length; i++){
+            if(bank[i]==null){
+                bank[i] = h;
+                break;
+            } } }
 
     public void removeHeld(){
         for(int i = 0; i<bank.length; i++) bank[i] = bank[i+1];
