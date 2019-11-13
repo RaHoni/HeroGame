2800070c18a1e5c18a11fd9e822d0a59cba3ff7d
RaHoni <honi2002suess@gmail.com>
2019-11-13 09:10:18 +0100

    Revert "Did some beautiful upgrades ..."
    This reverts commit 37392dc8

--- src/HeldenSpiel/Main.java
+++ src/HeldenSpiel/Main.java
@@ -1,7 +1,10 @@
 package HeldenSpiel;
 
+import HeldenSpiel.ownQueue.Queue2;
+
 class Main {
     public static void main(String[] args) {
-
+        Queue2 test = new Queue2();
+        System.out.println(test.isEmpty());
     }
 }
--- src/HeldenSpiel/Weapon.java
+++ src/HeldenSpiel/Weapon.java
@@ -1,38 +1,36 @@
 package HeldenSpiel;
 
+/**
+ * @author RaHoni und Cat
+ * @version 0.1
+ */
+public class Weapon
+{
+    // Bezugsobjekte
 
-public class Weapon {
+    // Attribute
     private int bonus;
     private String material;
     private int magic;
 
-    public Weapon(int pBo, String pMat, int pMag) {
+    // Konstruktor
+    public Weapon(int pBo, String pMat, int pMag)
+    {
         bonus = pBo;
         material = pMat;
         magic = pMag;
     }
 
-    public void setBonus(int pBo) {
-        bonus = pBo;
-    }
+    // Dienste
+    public void setBonus(int pBo) {bonus = pBo;}
+    
+    public int getBonus() {return bonus;}
+    
+    public void setMaterial(String pMat) {material = pMat;}
+    
+    public String getMaterial() {return material;}
 
-    public int getBonus() {
-        return bonus;
-    }
-
-    public void setMaterial(String pMat) {
-        material = pMat;
-    }
-
-    public String getMaterial() {
-        return material;
-    }
-
-    public void setMagic(int pMag) {
-        magic = pMag;
-    }
-
-    public int getMagic() {
-        return magic;
-    }
+    public void setMagic(int pMag) {magic = pMag;}
+    
+    public int getMagic() {return magic;}
 }
